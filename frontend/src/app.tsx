47a9951653ff51521935c5b3cf9ac85af6b43bf9
gerenko <gerenko@obit.ru>
2019-08-21 10:59:49 +0300

    The home page moved to a separate file

--- frontend/src/app.tsx
+++ frontend/src/app.tsx
@@ -2,22 +2,14 @@ import React from 'react';
 import { BrowserRouter as Router, Switch } from 'react-router-dom';
 
 import { LayoutRoute, Default } from './layouts';
+import { Home } from './pages';
 import './scss/style.scss';
-
-const HomePage: React.FC = () => {
-  return <div>Home</div>;
-};
 
 const App: React.FC = () => {
   return (
     <Router>
       <Switch>
-        <LayoutRoute
-          path="/"
-          layout={Default}
-          component={HomePage}
-          exact={true}
-        />
+        <LayoutRoute path="/" layout={Default} component={Home} exact={true} />
       </Switch>
     </Router>
   );
